04955e5a012a8f191773194ced7e82d37e388ea4
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-07-17 19:00:23 +0000

    tweak minor content

--- validator.js
+++ validator.js
@@ -1,22 +1,54 @@
-
-
-
-async function validate(email, msgError) {
-  
-
-//email
-  if (email && email.length < 3 || typeof (email) !== "string") {
-    return msgError.push({ "email": email + " is invalid" })
-}
-else if (email && !email.includes('@')) {
-  return msgError.push({ "email": email + " is invalid" })
-}
-
-
+async function validateEmail(email, msgError) {
+  //email
+  if (email == undefined || email.length < 3 || typeof email !== "string") {
+    if (!("email" in msgError)) {
+      msgError["email"] = [];
+    }
+    return msgError["email"].push("*Mandatory field!");
+  } else if (email == undefined || !email.includes("@")) {
+    if (!("email" in msgError)) {
+      msgError["email"] = [];
+    }
+    return msgError["email"].push({ email: email + " is invalid" });
+  }
 }
 
+async function validateNotEmptyString(data, dataName, msgError) {
+  //email
+
+  if (data == undefined || data == "" || typeof data !== "string") {
+    if (!(dataName in msgError)) {
+      msgError[dataName] = [];
+    }
+    return msgError[dataName].push("*Mandatory field!");
+  }
+}
+
+async function validateNotEmptyList(data, dataName, msgError) {
+  //email
+  if (data == undefined || data.length == 0 || typeof data !== "object") {
+    if (!(dataName in msgError)) {
+      msgError[dataName] = [];
+    }
+
+    return msgError[dataName].push("*Data is invalid or empty!");
+  }
+}
+
+async function validateNotNumber(data, dataName, msgError) {
+  //email
+  if (data == undefined || dataName > 0 || typeof data !== "number") {
+    if (!(dataName in msgError)) {
+      msgError[dataName] = []; // if key does not exist in msgError, add the key with an empty array.
+    }
+
+    return msgError[dataName].push("*Invalid number value!");
+  }
+}
 
 module.exports = {
-validate
- 
-}
+  validateEmail,
+  validateNotEmptyString,
+  validateNotEmptyList,
+  validateNotNumber,
+};
